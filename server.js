38c7fb2efa01b9a13e9b39ac61353a6e2f653cfe
saurabh <saurabh@gmail.com>
2021-03-04 15:39:12 +0530

    New

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config()
-// }
+
+require('./db')
 require('dotenv').config()
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-
+const PORT = 5000
 const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
@@ -14,16 +13,19 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL2, { useNewUrlParser: true})
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL2, { useNewUrlParser: true})
 
-const db = mongoose.connection
-db.on('error',(err)=>{
-    console.error("connection error please do check;", err);
-});
-db.once('open', () => console.log('connected to mongoose'))
+// const db = mongoose.connection
+// db.on('error',(err)=>{
+//     console.error("connection error please do check;", err);
+// });
+// db.once('open', () => console.log('connected to mongoose'))
 
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 5000)
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`)
+  })
+  
