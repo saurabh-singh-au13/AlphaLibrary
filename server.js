aad45a4002525c44328b6c7ff20c32d2fa8f1ae0
saurabh <saurabh@gmail.com>
2021-03-04 12:43:51 +0530

    Initial_Stage_of_Alpha_Library

---
+++ server.js
@@ -0,0 +1,29 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+
+const db = mongoose.connection
+db.on('error',(err)=>{
+    console.error("connection error;", err);
+});
+db.once('open', () => console.log('connected to mongoose'))
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 4000)
