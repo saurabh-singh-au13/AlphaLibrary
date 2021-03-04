e749fc3023775d28a2d9a837f0359886ca0dff31
saurabh <saurabh@gmail.com>
2021-03-04 14:44:43 +0530

    Server.js Modified

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
-
+// if(process.env.NODE_ENV !== 'production'){
+//     require('dotenv').config()
+// }
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -15,7 +15,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL2, { useNewUrlParser: true})
 
 const db = mongoose.connection
 db.on('error',(err)=>{
@@ -26,4 +26,4 @@ db.once('open', () => console.log('connected to mongoose'))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 5000)
